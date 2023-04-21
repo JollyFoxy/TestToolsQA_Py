a2bc4e62d0bdc98b23becf67949afb81cd6ed9e7
pavel <pavel.mizirev@gmail.com>
2023-04-21 17:43:40 +0300

    ту-ту2

--- pages/widgets/page_menu.py
+++ pages/widgets/page_menu.py
@@ -1,3 +1,5 @@
+import time
+
 import allure
 
 from pages.page_base import BasePage
@@ -10,19 +12,10 @@ class PageMenu(BasePage):
     @allure.step("")
     def step_1_transition(self):
         self._first_element_transition("Widgets")
-        Scrolling(self._driver).scroll_to(y=400)
+        Scrolling(self._driver).scroll_to(y="4000")
         self._second_element_transition("Menu")
 
     @allure.step("")
-    def step_2_hover_main_item2(self):
-        main_item2 = A("//a[.='Main Item 2']", self._driver)
-        main_item2.hover_to_a()
-    @allure.step("")
-    def step_3_hover_sub_list(self):
-        sub_list = A("//a[.='SUB SUB LIST »']", self._driver)
-        sub_list.hover_to_a()
-    @allure.step("")
-    def step_4_hover_sub_sub_item2(self):
-        sub_sub_item2 = A("//a[.='Sub Sub Item 2']", self._driver)
-        sub_sub_item2.hover_to_a()
-
+    def step_hover_to_element_menu(self, element: str):
+        el_a = A(f"//a[.='{element}']", self._driver)
+        el_a.hover_to_a()
---
+++ tests/widgets/test_menu.py
@@ -0,0 +1,9 @@
+from pages.widgets.page_menu import PageMenu
+
+
+def test_menu(get_driver):
+    _steps = PageMenu(get_driver)
+    _steps.step_1_transition()
+    _steps.step_hover_to_element_menu("Main Item 2")
+    _steps.step_hover_to_element_menu("SUB SUB LIST »")
+    _steps.step_hover_to_element_menu("Sub Sub Item 2")
