a9ac9091e3aeacd5e3a3f1e41b97ecfdc8402945
pavel <pavel.mizirev@gmail.com>
2023-05-02 17:25:54 +0300

    забыл

--- api_steps/post/steps_post_create_user.py
+++ api_steps/post/steps_post_create_user.py
@@ -5,7 +5,7 @@ from api_steps.base_api import BaseApi
 
 class StepsPostCreateUser(BaseApi):
 
-    @allure.step("")
+    @allure.step("Отправить post запрос по URL(https://reqres.in/api/users)")
     def post_create_user(self, url: str):
         body = {
             "name": "morpheus",
